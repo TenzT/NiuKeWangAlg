cd04890bbf90d9c0cf3e37bea9bea8223eecbcf3
“Tenz” <tenz@sina.cn>
2019-02-11 15:23:51 +0800

    链表荷兰国旗问题

---
+++ src/CH03/SmallerEqualBigger.java
@@ -0,0 +1,86 @@
+package CH03;
+
+/**
+ * 将单向链表按某值划分成左边小、 中间相等、 右边大的形式
+ * 思路：如果没有限制空间，则先转成数组、用荷兰国旗问题解决后再重构链表
+ */
+public class SmallerEqualBigger {
+	public static class Node {
+		public int value;
+		public Node next;
+
+		public Node(int data) {
+			this.value = data;
+		}
+	}
+
+	public static Node listPartition1(Node head, int pivot) {
+		if (head == null) {
+			return head;
+		}
+		Node cur = head;
+		int i = 0;
+		while (cur != null) {
+			i++;
+			cur = cur.next;
+		}
+		Node[] nodeArr = new Node[i];
+		i = 0;
+		cur = head;
+		for (i = 0; i != nodeArr.length; i++) {
+			nodeArr[i] = cur;
+			cur = cur.next;
+		}
+		arrPartition(nodeArr, pivot);
+		for (i = 1; i != nodeArr.length; i++) {
+			nodeArr[i - 1].next = nodeArr[i];
+		}
+		nodeArr[i - 1].next = null;
+		return nodeArr[0];
+	}
+	
+	// 荷兰国旗问题
+	public static void arrPartition(Node[] nodeArr, int pivot) {
+		int small = -1;
+		int big = nodeArr.length;
+		int index = 0;
+		while (index != big) {
+			if (nodeArr[index].value < pivot) {
+				swap(nodeArr, ++small, index++);
+			} else if (nodeArr[index].value == pivot) {
+				index++;
+			} else {
+				swap(nodeArr, --big, index);
+			}
+		}
+	}
+	public static void swap(Node[] nodeArr, int a, int b) {
+		Node tmp = nodeArr[a];
+		nodeArr[a] = nodeArr[b];
+		nodeArr[b] = tmp;
+	}
+	
+	public static void printLinkedList(Node node) {
+		System.out.print("Linked List: ");
+		while (node != null) {
+			System.out.print(node.value + " ");
+			node = node.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Node head1 = new Node(7);
+		head1.next = new Node(9);
+		head1.next.next = new Node(1);
+		head1.next.next.next = new Node(8);
+		head1.next.next.next.next = new Node(5);
+		head1.next.next.next.next.next = new Node(2);
+		head1.next.next.next.next.next.next = new Node(5);
+		printLinkedList(head1);
+		 head1 = listPartition1(head1, 4);
+//		head1 = listPartition2(head1, 5);	// 还没实现
+		printLinkedList(head1);
+
+	}
+}
